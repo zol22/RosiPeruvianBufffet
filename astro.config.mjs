19f98eb4d4437be10b65b1bb0730eba19b5c0516
WP Infusion <info@wpinfusion.com>
2024-12-22 23:05:57 +0100

    add critical css

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import alpinejs from "@astrojs/alpinejs";
 
 import mdx from "@astrojs/mdx";
 
+import criticalCss from "astro-critical-css";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), alpinejs(), mdx()],
+    integrations: [tailwind(), alpinejs(), mdx(), criticalCss()],
     output: "static",
     devToolbar: {
         enabled: false,
